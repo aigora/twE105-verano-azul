a1152a4f835655db6f6b49ad31ac188d9fb8add2
marfperez <47813387+marfperez@users.noreply.github.com>
2019-04-05 13:51:47 +0200

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+void main()
+{
+char c,i,s,p ;
+int opcion;
+printf("Bienvenido a La Papeleria Nogales: ");
+printf("Escoga una opcion (en minusculas) entre:\n Catalogo (c)\n Informacion (i)\n Sugerencias (s)\n Cuenta personal (p)\n");
+scanf(" %c", &opcion);
+switch(opcion)
+{
+case 'c':
+printf("catalogo\n");
+break;
+case 'i':
+printf("Informacion:\n Direccion: Calle Alto del Arenal,8 Madrid \n Telefono: 912-854-493 \n Email: papelerianogales@gmail.com \n");
+break;
+case 's': 
+printf("Sugerecias\n");
+break;
+case 'p':
+printf("Cuenta personal\n");
+break;
+
+default:
+printf("Letra incorrecta\n");
+}
+}
+
